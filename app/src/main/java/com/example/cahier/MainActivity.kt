b747ef9d3c6565df6281b0e04b6c8bcbfc53d6c7
Courtlyn Boykin <courtlynb@google.com>
2024-02-27 10:30:10 -0500

    Added SplashScreen using SplashScreen API

--- app/src/main/java/com/example/cahier/MainActivity.kt
+++ app/src/main/java/com/example/cahier/MainActivity.kt
@@ -10,11 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.cahier.ui.theme.CahierTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             CahierTheme {
                 // A surface container using the 'background' color from the theme
